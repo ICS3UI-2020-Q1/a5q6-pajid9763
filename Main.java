d09e027471e6fa4b1b683432187eac4356f7db85
pajid9763 <pajid9763@wrdsb.ca>
2020-10-06 17:10:55 +0000

    finished program that print a box of stars

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that prints a box of stars
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // ask them for a number between 1-10
+    System.out.println("Please enter a number between 1 and 10");
+    int num = input.nextInt();
+    
+    for (int i = 1; i <= num; i++){
+
+      for (int j = 1; j <= num; j++){
+        System.out.print("*");
+      
+    }
+    System.out.println("");
+    }
     
   }
 }
